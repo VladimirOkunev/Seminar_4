c81d94120f32069e623f6087ad733698f6b6279b
Vladimir_Okunev <vova-okunev@mail.ru>
2022-11-12 18:54:18 +0900

    added task 25

---
+++ Task_25/Program.cs
@@ -0,0 +1,25 @@
+﻿// Напишите цикл, который принимает на вход два числа (A и B) 
+// и возводит число A в натуральную степень B
+
+Console.Clear();
+
+int Squar(int number, int power)
+{
+    int sqr = 1;
+
+    for(int i = 0; i < power; i++)
+    {
+        sqr *= number;
+    }
+    return sqr;
+}
+
+Console.Write("Enter the first number: ");
+int numA = int.Parse(Console.ReadLine() ?? "0");
+
+Console.Write("Enter the second number: ");
+int numB = int.Parse(Console.ReadLine() ?? "0");
+
+int sqr = Squar(numA, numB);
+
+Console.Write($"{numA} in power {numB} is {sqr}");
