849797798a4a7cea4fb4fe6444eb8f09b7b66a76
Vladimir_Okunev <vova-okunev@mail.ru>
2022-11-12 19:07:37 +0900

    added task 27

---
+++ Task_27/Program.cs
@@ -0,0 +1,23 @@
+﻿// Напишите программу, которая принимает на вход число
+// и выдаёт сумму цифр в числе.
+
+Console.Clear();
+
+int Sum (int num)
+{
+    int sum = 0;
+    while(num > 0)
+    {
+        sum += num % 10;
+        num = num / 10;
+    }
+
+    return sum;
+}
+
+Console.Write("Enter a number: ");
+int number = int.Parse(Console.ReadLine() ?? "0");
+
+int sum = Sum(number);
+
+Console.Write($"Sum of all digits of number {number} is {sum}");
